310aed27e90fa83310a5a8b9a3eb734e465cbe9d
Andries Nieuwenhuize <nieu0082@hz.nl>
2020-09-02 13:21:28 +0200

    groovy added

---
+++ app/src/androidTest/java/com/balaganovrocks/yourmasterclean/adapter/BatterySavingAdapterTest.groovy
@@ -0,0 +1,13 @@
+package com.balaganovrocks.yourmasterclean.adapter
+
+class BatterySavingAdapterTest extends GroovyTestCase {
+    void testGetCount() {
+
+    }
+
+    void testGetItem() {
+    }
+
+    void testGetItemId() {
+    }
+}
